2d15ee410eb1eb1499c72f42cfa5fae592e6c73a
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-03-15 20:46:18 -0500

    refactored to use pandas

--- Day 26 - Phonetic Alphabet/main.py
+++ Day 26 - Phonetic Alphabet/main.py
@@ -1,9 +1,11 @@
-with open("nato_phonetic_alphabet.csv") as f:
-    phonetics_list = f.read().split()[1:]
-
-phonetics_list = [item.split(",") for item in phonetics_list]
-phonetics_dict = {item[0]: item[1] for item in phonetics_list}
-
+# with open("nato_phonetic_alphabet.csv") as f:
+#     phonetics_list = f.read().split()[1:]
+#
+# phonetics_list = [item.split(",") for item in phonetics_list]
+# phonetics_dict = {item[0]: item[1] for item in phonetics_list}
+import pandas
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetics_dict = {value.letter: value.letter for (key, value) in df.iterrows()}
 word = input("Enter a word: ")
 word_in_phonetics = [phonetics_dict[x] for x in word.upper() if x.isalpha()]
 print(word_in_phonetics)
